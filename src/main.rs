924d6433203c356f519f94a5650fbcd5dda684f5
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2023-03-02 15:18:48 -0500

    Backup before picking it up again.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,48 @@
 // https://www.a1k0n.net/2011/07/20/donut-math.html
-use std::f32::consts::PI;
+//use std::f32::consts::PI;
 use std::cmp::{
     max,
     min,
 };
+use std::{
+    thread,
+    time::Duration,
+};
+
 
 fn main() {
-    const theta_spacing: usize = 7;
-    const phi_spacing:   usize = 2;
+    const THETA_SPACING: usize = 7;  // 0.07
+    const PHI_SPACING:   usize = 2;  // 0.02
 
     const R1: f32 = 1.;
     const R2: f32 = 2.;
     const K2: f32 = 5.;
 
-    const screen_width:  usize = 80;
-    const screen_height: usize = 22;
+    const SCREEN_WIDTH:  usize = 80;
+    const SCREEN_HEIGHT: usize = 30;
 
-    const K1: f32 = screen_width as f32 * K2 * 3. / (8. * (R1 + R2));
+    const K1: f32 = SCREEN_WIDTH as f32 * K2 * 3. / (8. * (R1 + R2));
 
     let mut A: f32=0.;
     let mut B: f32=0.;
 
-    println!("TEST");
     print!("\x1b[2J");
     loop {
-        let cosA: f32 = A.cos();
-        let sinA: f32 = A.sin();
-        let cosB: f32 = B.cos();
-        let sinB: f32 = B.sin();
-        let mut output  = [[' '; screen_height]; screen_width];
-        let mut zbuffer = [[0.; screen_height]; screen_width];
+        let cos_a: f32 = A.cos();
+        let sin_a: f32 = A.sin();
+        let cos_b: f32 = B.cos();
+        let sin_b: f32 = B.sin();
+        let mut output  = [[' '; SCREEN_HEIGHT]; SCREEN_WIDTH];
+        let mut zbuffer = [[0.; SCREEN_HEIGHT]; SCREEN_WIDTH];
 
-        for theta in (0usize..628).step_by(theta_spacing) {
-            let theta = theta as f32 / 100.;
+        for _theta in (0usize..628).step_by(THETA_SPACING) {
+            let theta = _theta as f32 / 100.;
             let costheta = theta.cos();
             let sintheta = theta.sin();
 
-            for phi in (0usize..628).step_by(phi_spacing) {
-                let phi = phi as f32 / 100.;
+            for _phi in (0usize..628).step_by(PHI_SPACING) {
+                let phi = _phi as f32 / 100.;
+                //println!("{:?} {:?}", theta, phi);
                 let cosphi = phi.cos();
                 let sinphi = phi.sin();
 
@@ -46,24 +51,26 @@ fn main() {
                 let circlex: f32 = R2 + R1 * costheta;
                 let circley: f32 = R1 * sintheta;
                 
-                let x = circlex * (cosB*cosphi + sinA*sinB*sinphi) - circley*cosA*sinB;
-                let y = circlex * (sinB*cosphi - sinA*cosB*sinphi) + circley*cosA*cosB;
-                let z = K2 + cosA*circlex*sinphi + circley*sinA;
+                let x = circlex * (cos_b*cosphi + sin_a*sin_b*sinphi)
+                    - circley*cos_a*sin_b;
+                let y = circlex * (sin_b*cosphi - sin_a*cos_b*sinphi)
+                    + circley*cos_a*cos_b;
+                let z = K2 + cos_a*circlex*sinphi + circley*sin_a;
                 let ooz = 1. / z;  // one over z
 
-                let xp: usize = (screen_width/2) as usize + (K1*ooz*x) as usize;
-                //println!("{:?} - {:?}", screen_height/2, K1*ooz*y);
-                let yp: isize = (screen_height/2) as isize - (K1*ooz*y) as isize;
-                let yp: usize = min((screen_height - 1) as isize, max(0isize, yp)) as usize;
+                let _xp: isize = (SCREEN_WIDTH/2) as isize + (K1*ooz*x) as isize;
+                let xp: usize = min((SCREEN_WIDTH - 1) as isize, max(0isize, _xp)) as usize;
+                let _yp: isize = (SCREEN_HEIGHT/2) as isize - (K1*ooz*y) as isize;
+                let yp: usize = min((SCREEN_HEIGHT - 1) as isize, max(0isize, _yp)) as usize;
 
-                let L: f32 = cosphi*costheta*sinB
-                    - cosA*costheta*sinphi
-                    - sinA*sintheta
-                    + cosB*(cosA*sintheta - costheta*sinA*sinphi);
-                if L > 0. {
+                let luminance: f32 = cosphi*costheta*sin_b
+                    - cos_a*costheta*sinphi
+                    - sin_a*sintheta
+                    + cos_b*(cos_a*sintheta - costheta*sin_a*sinphi);
+                if luminance > 0. {
                     if ooz > zbuffer[xp][yp] {
                         zbuffer[xp][yp] = ooz;
-                        let luminance_index = L as usize * 8;
+                        let luminance_index = (luminance * 8.) as usize;
                         output[xp][yp] = ".,-~:;=!*#$@"
                             .chars()
                             .nth(luminance_index)
@@ -74,14 +81,18 @@ fn main() {
         }
 
         print!("\x1b[H");
-        for j in 0..screen_height {
-            for i in 0..screen_width {
-                print!("{}", output[i][j]);
+        if true {
+            for j in 0..SCREEN_HEIGHT {
+                for i in 0..SCREEN_WIDTH {
+                    print!("{}", output[i][j]);
+                }
+                print!("\n");
             }
-            print!("\n");
         }
 
         A += 0.00004;
         B += 0.00002;
+
+        thread::sleep(Duration::from_millis(40));
     };
 }
